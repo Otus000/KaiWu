b0140c06868e9113eab91fdcde9337835fd188ee
Otus <965868276@qq.com>
2022-09-16 16:22:07 +0800

    dual-clip PPO

--- code/common/algorithm.py
+++ code/common/algorithm.py
@@ -320,15 +320,15 @@ class Algorithm:
                     )
                     * advantage
                 )
-                # temp_policy_loss = -tf.reduce_sum(
-                #     tf.to_float(weight_list[task_index]) * tf.minimum(surr1, surr2)
-                # ) / tf.maximum(tf.reduce_sum(tf.to_float(weight_list[task_index])), 1.0)
-                # dual-clip PPO
-                clip = tf.minimum(surr1, surr2)
-                clip = tf.where(tf.math.less(advantage, 0), tf.maximum(clip, 3.0 * advantage), clip)
                 temp_policy_loss = -tf.reduce_sum(
-                    tf.to_float(weight_list[task_index]) * clip
+                    tf.to_float(weight_list[task_index]) * tf.minimum(surr1, surr2)
                 ) / tf.maximum(tf.reduce_sum(tf.to_float(weight_list[task_index])), 1.0)
+                # # dual-clip PPO
+                # clip = tf.minimum(surr1, surr2)
+                # clip = tf.where(tf.math.less(advantage, 0), tf.maximum(clip, 3.0 * advantage), clip)
+                # temp_policy_loss = -tf.reduce_sum(
+                #     tf.to_float(weight_list[task_index]) * clip
+                # ) / tf.maximum(tf.reduce_sum(tf.to_float(weight_list[task_index])), 1.0)
                 self.policy_cost = self.policy_cost + temp_policy_loss
         # cross entropy loss
         current_entropy_loss_index = 0
